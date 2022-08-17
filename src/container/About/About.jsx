c41cd5295fbbff24eb0a33f3ad6c676ef47e79dd
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-08-16 20:13:30 -0400

    connection with client for about section content

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,38 +3,24 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-  {
-    title: 'Web Development',
-    description: `I'm a good web developer, I'm always looking for ways to clean, automate and reuse my code.`,
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Web Design',
-    description: `I am learning about the architectures of web pages in order to find the most convenient design when creating them.`,
-    imgUrl: images.about02,
-  },
-  {
-    title: 'UI/UX',
-    description: `It's an area that I haven't touched much yet but I'm dying to study and apply, since I believe that the user's experience on a site is everything.`,
-    imgUrl: images.about03,
-  },
-  {
-    title: 'Web Animations',
-    description: `I'm good at integrating animations into my sites.`,
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
         {' '}
         I know That
         <span> Good Design </span>
-        and  <span>Development </span>
+        and <span>Development </span>
         <br />
         Means
         <span> Good Business</span>
@@ -49,7 +35,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
